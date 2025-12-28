42d6de194ca84e42c074a145d9e72d4b510aca19
KustelR <dmitriysstudios2@gmail.com>
2025-12-28 16:22:39 +0400

    made data from config uppercase because it's kinda constant i guess?

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ from mcstatus import JavaServer
 from api import getReport, writeReport
 
 
-target = "localhost:25565"
-output_file = "report"
-interval = 60  # seconds
-
-
 def createReport():
     report = getReport(server)
-    writeReport(output_file, target, report)
+    writeReport(OUTPUT_FILE, TARGET, report)
     print(f"Report created at {report.timestamp}, {report.online} players online.")
-    scheduler.enter(interval, 1, createReport)
+    scheduler.enter(INTERVAL, 1, createReport)
+
+
+TARGET = "localhost:25565"
+OUTPUT_FILE = "report"
+INTERVAL = 60  # seconds
 
 
 config = yaml.safe_load(open("config.yaml", "r"))
-output_file = config["output"]
-interval = config["interval"]
-target = config["target"]
+OUTPUT_FILE = config["output"]
+INTERVAL = config["interval"]
+TARGET = config["target"]
 
-server = JavaServer.lookup(target)
+server = JavaServer.lookup(TARGET)
 scheduler = sched.scheduler(time.time, time.sleep)
 
 scheduler.enter(1, 1, createReport)
